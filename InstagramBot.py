ff4807bea94ebd7d230ce4034cd6e87b52f3b242
Akash_Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-10-27 15:17:01 +0530

    Add files via upload

---
+++ InstagramBot.py
@@ -0,0 +1,14 @@
+# from instabot import Bot
+# bot=Bot()
+# bot.login(username="your username",passward="your password")
+# bot.follow("Akash_Raut3760")
+# bot.upload_photo("give the path of pic and picname")
+# bot.unfollow("following person name")
+# bot.send_message("I love python,["person names for sending msg"])
+# followers=bot.get_user_followers("follower name")
+# for follower in followers:
+#     print(bot.get_user_info(followerpersongs name))
+# following=bot.get_user_following("following person name")
+# for following in the following:
+#     print(bot.get_user_info("FollowingPersons"))
+
